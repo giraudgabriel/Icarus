b4621682ddb3da27495aab7d67def3cc719846ac
Gabriel Fernandes Giraud <gabriel.giraud@fatec.sp.gov.br>
2019-11-13 14:22:47 -0300

    #FEATURE: Inicio do frontend.

---
+++ frontend/src/components/Home.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <Menu></Menu>
+        <div class="row" id="main">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md">
+                        <button class="btn btn-warning btn-block btn-lg">Projetos</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>    
+</template>
+
+<script>
+import Menu from "./Menu";
+
+export default {
+    name:'home',
+    components:{
+        Menu
+    }
+}
+</script>
+
+<style scoped>
+#main{
+    padding-top:70px;
+}
+</style>
---
+++ frontend/src/components/Menu.vue
@@ -0,0 +1,50 @@
+<template>
+    <div>
+        <div>
+
+            <b-navbar toggleable="lg" type="dark" fixed="top" variant="info">
+                <b-navbar-brand href="#"><img class="logo" src="../assets/logo.png" alt=""></b-navbar-brand>
+
+                <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+                <b-collapse id="nav-collapse" is-nav>
+                    <b-navbar-nav>
+                        <b-nav-item href="#"> <i class="fa fa-building"></i> Projetos</b-nav-item>
+                        <b-nav-item href="#"> <i class="fa fa-info"></i> Sobre nós</b-nav-item>
+                    </b-navbar-nav>
+
+                    <b-navbar-nav class="ml-auto">
+
+                        <b-nav-item-dropdown right>
+                            <template v-slot:button-content>
+                               <i class="fa fa-globe"></i> Idioma
+                            </template>
+                            <b-dropdown-item href="#">PT-BR</b-dropdown-item>
+                        </b-nav-item-dropdown>
+
+                        <b-nav-item-dropdown right>
+                            <template v-slot:button-content>
+                               <i class="fa fa-user"></i> Entrar
+                            </template>
+                            <b-dropdown-item href="#"><i class="fa fa-list"></i> Perfil</b-dropdown-item>
+                            <b-dropdown-item href="#"><i class="fa fa-power-off"></i> Sair</b-dropdown-item>
+                        </b-nav-item-dropdown>
+                    </b-navbar-nav>
+                </b-collapse>
+            </b-navbar>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'menu'
+    }
+</script>
+
+<style coped>
+    .logo {
+        width: 70px;
+        height: 50px;
+    }
+</style>
